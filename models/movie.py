19c8fd4e46d4d40716e1dd35e3dbdcb723d15750
Subhrajyoti <subha.ionic3@gmail.com>
2020-12-12 01:54:33 +0530

    change movie model's Type required to not required

--- models/movie.py
+++ models/movie.py
@@ -9,7 +9,7 @@ class MovieModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Title = db.Column(db.String(100), nullable=False)
     Poster = db.Column(db.String, nullable=False)
-    Type = db.Column(db.String(10), nullable=False)
+    Type = db.Column(db.String(10))
     Year = db.Column(db.String(5), nullable=False)
     imdbID = db.Column(db.String(20), nullable=False, unique=True)
     isFav = db.Column(db.Boolean)
